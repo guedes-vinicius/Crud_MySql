e21c7dfd8354ecaf7d8e6346f6fe0d7a29c5b832
Vinicius <viniciusguedes63@gmail.com>
2021-03-23 13:30:10 -0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/contador_estoque/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contador_estoque
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'home_page_controler.dart';
+
+class HomePage extends StatelessWidget{
+  HomePage(){
+    //Get.put(HomePageController()); //Criar controlador
+  }
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Estoque"),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.sort) ,
+              tooltip: "Ordenar",
+        onPressed: (){}
+        ),
+          IconButton(icon: const Icon(Icons.search),
+              tooltip: "Pesquisar",
+              onPressed: (){}
+              ),
+          IconButton(icon: const Icon(Icons.settings_applications), onPressed: (){})
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+// codigo, nome, codigo de barras,
+//list view itens, page control
