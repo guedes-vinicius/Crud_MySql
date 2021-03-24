c7861662667898b6e25933ec12ee6ffe07850dc7
vinicius <viniciusguedes63@gmail.com>
2021-03-24 20:48:34 -0300

    layout home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,35 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'home_page_controler.dart';
 
-class HomePage extends StatelessWidget{
-  HomePage(){
-    //Get.put(HomePageController()); //Criar controlador
+class HomePage extends StatelessWidget {
+  HomePage() {
+    Get.put(HomePageController());
   }
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Estoque"),
-        actions: <Widget>[
-          IconButton(icon: const Icon(Icons.search_rounded) ,
-              tooltip: "Pesquisar",
-        onPressed: (){}
-        ),
-          IconButton(icon: const Icon(Icons.sort_rounded),
-              tooltip: "Ordenar",
-              onPressed: (){}
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        initialIndex: 1,
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Estoque"),
+            backgroundColor: Color(0xffff0000),
+            actions: <Widget>[
+              IconButton(
+                  icon: const Icon(Icons.search_rounded),
+                  tooltip: "Pesquisar",
+                  onPressed: () {}),
+              IconButton(
+                  icon: const Icon(Icons.sort_rounded),
+                  tooltip: "Ordenar",
+                  onPressed: () {}),
+              IconButton(
+                  icon: const Icon(Icons.description_sharp),
+                  tooltip: "Exportar",
+                  onPressed: () {})
+            ],
+            bottom: TabBar(
+                indicatorWeight: 3.0,
+                indicatorColor: Colors.white,
+                tabs: <Widget>[
+                  Tab(
+                    text: "Contagem",
+                  ),
+                  Tab(
+                    text: "Produtos",
+                  )
+                ]),
+          ),
+          body: SizedBox.expand(
+              child: Padding(
+            padding: const EdgeInsets.fromLTRB(250.0, 380.0, 0.0, 0.0),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  IconButton(
+                      icon: Image.asset(
+                        'assets/icon.png',
+                      ),
+                      iconSize: 50,
+                      onPressed: () => Get.find<HomePageController>()
+                          .escanearCodigoBarras()),
+                  Expanded(
+                    child: IconButton(
+                      icon: Image.asset('assets/more.png'),
+                      padding: const EdgeInsets.fromLTRB(250.0, 500.0, 0.0, 0.0),
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
+                  )
+                ],
               ),
-          IconButton(icon: const Icon(Icons.expand_more_rounded),
-              tooltip: "Mais",
-              onPressed: (){})
-        ],
-      ),
-    );
+            ),
+          )),
+        ));
   }
 }
 
 
 
-// codigo, nome, codigo de barras,
+
 //list view itens, page control(page view)
